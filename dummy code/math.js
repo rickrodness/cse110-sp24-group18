7263b5ef395c5edb0035539581e2a2de750fa933
Michael Cheung Ka Ming <michaelcheungkm@gmail.com>
2024-05-09 19:45:18 -0700

    testing update

--- dummy code/math.js
+++ dummy code/math.js
@@ -6,7 +6,7 @@
  * @param {number} b - The second number.
  * @returns {number} The sum of the two numbers.
  */
-function add(a, b) {
+export function add(a, b) {
   return a + b;
 }
 
@@ -16,8 +16,6 @@ function add(a, b) {
  * @param {number} b - The second number.
  * @returns {number} The result of subtracting the second number from the first.
  */
-function subtract(a, b) {
+export function subtract(a, b) {
   return a - b;
 }
-
-module.exports = { add, subtract };
