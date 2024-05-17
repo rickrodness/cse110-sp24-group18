f9b47be87c512ce3656d8fc6b69b17fd437ebce3
rseidl25 <rseidl@ucsd.edu>
2024-05-17 14:47:50 -0700

    commented html

--- emotion-widget/emotion-widget.js
+++ emotion-widget/emotion-widget.js
@@ -1,48 +1,5 @@
-/*document.getElementById('emotionSlider').addEventListener('input', function() {
-
-  const arrowContainer = document.getElementById('arrowContainer');
-  const faceImg = document.getElementById('face');
-  const emotionText = document.getElementById('emotionText');
-
-  // Convert the value of the input (this.value) to an integer
-  const value = parseInt(this.value);
-
-  switch(value){
-    case 1:
-      arrowContainer.style.left = '19%';
-      faceImg.src="media/faceMiserable.png";
-      emotionText.textContent = 'Miserable';
-      break;
-    case 2:
-      arrowContainer.style.left = '35%';
-      faceImg.src="media/faceSad.png";
-      emotionText.textContent = 'Sad';
-      break;
-    case 3:
-      arrowContainer.style.left = '51.5%';
-      faceImg.src="media/faceMeh.png";
-      emotionText.textContent = 'Meh';
-      break;
-    case 4:
-      arrowContainer.style.left = '68%';
-      faceImg.src="media/faceGood.png";
-      emotionText.textContent = 'Happy';
-      break;
-    case 5:
-      arrowContainer.style.left = '84%';
-      faceImg.src="media/faceAmazing.png";
-      emotionText.textContent = 'Amazing';
-      break;
-    default:
-      arrowContainer.style.left = '51.5%';
-      faceImg.src="media/faceMeh.png";
-      emotionText.textContent = 'Meh';
-  }
-
-});*/
-
 const gauge = document.getElementById('gauge');
-/*const gaugeValue = document.getElementById('score');*/
+// const gaugeValue = document.getElementById('score');
 
 gauge.addEventListener('mousemove', (event) => {
   
@@ -67,7 +24,7 @@ gauge.addEventListener('mousemove', (event) => {
     // Only consider angles between 0 and 180 degrees
     if (angle >= 180 && angle <= 360) {
         let speed = (angle / 180) * 100;
-        /*gaugeValue.textContent = `${Math.round(speed) - 100}`;*/
+        // gaugeValue.textContent = `${Math.round(speed) - 100}`;
 
       if(speed >= 100 && speed < 120){
         faceImg.src="media/faceMiserable.png";
