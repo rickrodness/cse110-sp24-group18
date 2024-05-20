c3510b16805cfc996d48f7f831caf44298072ffc
BernicoJC <108056736+BernicoJC@users.noreply.github.com>
2024-05-20 15:52:28 -0700

    Delete dummyCode directory

--- dummyCode/tests/math.test.js
+++
@@ -1,9 +0,0 @@
-const { addtion, subtraction } = require('../math');
-
-test('adds 1 + 2 to equal 3', () => {
-  expect(addtion(1, 2)).toBe(3);
-});
-
-test('subtracts 5 - 3 to equal 2', () => {
-  expect(subtraction(5, 3)).toBe(2);
-});
