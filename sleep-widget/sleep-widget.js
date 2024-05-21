3eac6953c8db2cf762d9feaa66228e2c062899a4
Taiki-id31 <tayoshino@ucsd.edu>
2024-05-21 14:07:59 -0700

    vertical slider

--- sleep-widget/sleep-widget.js
+++ sleep-widget/sleep-widget.js
@@ -1,17 +1,26 @@
-const slider = document.getElementById("myRange");
-const output = document.getElementById("value");
-const labels = ["<3 Hours", "3-4 Hours", "5 Hours", "6-7 Hours", "7-9 Hours"];
-
 document.addEventListener('DOMContentLoaded', function() {
-  function updateSliderFill(value) {
-      const percentage = (value - slider.min) / (slider.max - slider.min) * 100;
-      slider.style.setProperty('--slider-value', `${percentage}%`);
-  }
-  output.innerHTML = labels[slider.value - 1];
-  updateSliderFill(slider.value);
-
+  const slider = document.querySelector('.slider');
   slider.oninput = function() {
-      output.innerHTML = labels[this.value - 1];
-      updateSliderFill(this.value);
-  };
+      const percentage = (this.value - this.min) / (this.max - this.min) * 100;
+      this.style.background = `linear-gradient(to right, #FF5C35 0%, #FF5C35 ${percentage}%, #e5dedb ${percentage}%, #e5dedb 100%)`;
+  }
+  slider.value = 3;  
+  slider.dispatchEvent(new Event('input')); 
+
+
+  //chanage a label color
+  const condition_labels = document.querySelectorAll('.label p');
+  const time_labels = document.querySelectorAll('.label span');
+  function updateLabelColor() {
+    condition_labels.forEach(label => {
+        label.classList.remove('highlight-color');
+    });
+    time_labels.forEach(label => {
+      label.classList.remove('highlight-color');
+  });
+    condition_labels[5-(slider.value)].classList.add('highlight-color');
+    time_labels[5-(slider.value)].classList.add('highlight-color');
+  }
+  updateLabelColor();
+  slider.addEventListener('input', updateLabelColor);
 });
