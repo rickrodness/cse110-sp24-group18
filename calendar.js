f00133085f46b9a35f97f9847c8dac7a7a738cde
hosaka-elijah <81043167+hosaka-elijah@users.noreply.github.com>
2024-05-20 17:00:09 -0700

    Update calendar widget

--- calendar.js
+++ calendar.js
@@ -1,36 +1,19 @@
-
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  const monthSelect = document.getElementById('Month');
-  const daySelect = document.getElementById('Day');
-  const yearSelect = document.getElementById('Year');
-  const selectDateButton = document.querySelector('button');
-  const output = document.getElementById('output');
+    const monthSelect = document.getElementById('Month');
+    const yearSelect = document.getElementById('Year');
+    const output = document.getElementById('output');
 
-  // Event listener for month selection
-  monthSelect.addEventListener('change', function() {
-    const selectedMonth = monthSelect.value;
-    return selectedMonth; 
-  });
+    // Event listener for month selection
+    monthSelect.addEventListener('change', updateOutput);
+    
+    // Event listener for year selection
+    yearSelect.addEventListener('change', updateOutput);
 
-   // Event listener for day selection
-  daySelect.addEventListener('change', function() {
-    const selectedDay = daySelect.value;
-    return selectedDay;
-  });
-
-    // Event listener for day selection
-  daySelect.addEventListener('change', function() {
-    const selectedYear = yearSelect.value;
-    return selectedYear;
-  });
-
-  // Event listener for select date button
-  selectDateButton.addEventListener('click', function() {
-    const selectedMonth = monthSelect.value;
-    const selectedDay = daySelect.value;
-    const selectedYear = yearSelect.value;
-    output.textContent = "Selected Date: " + selectedMonth + " " + selectedDay + ", " + selectedYear;
-  });
+    function updateOutput() {
+        const selectedMonth = monthSelect.value;
+        const selectedYear = yearSelect.value;
+        output.textContent = "Selected Date: " + selectedMonth + ", " + selectedYear;
+    }
 }
