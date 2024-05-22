2e1e62ce6507d8ad3007f0b0dcded3e461df3f84
BernicoJC <108056736+BernicoJC@users.noreply.github.com>
2024-05-21 23:56:11 -0700

    Making sure only one widget at a time

--- journal-nav-test/assets/scripts/widgets.js
+++ journal-nav-test/assets/scripts/widgets.js
@@ -1,14 +1,28 @@
 function widgetButtonListeners() {
   const widgetButtons = document.querySelectorAll(".widget-btn");
 
-  console.log(widgetButtons)
-
   widgetButtons.forEach((btn) => {
+    // make sure all the widgets start closed
+    const respectiveWidget = document.getElementById(btn.dataset.target);
+    respectiveWidget.style.display = "none";
+
     btn.addEventListener("click", () => {
       const targetWidget = document.getElementById(btn.dataset.target);
 
       if(targetWidget){
-        targetWidget.style.display = (targetWidget.style.display == "flex") ? "none":"flex";
+        // If widget is closed, open
+        if(targetWidget.style.display == "none"){
+          // close all the widgets
+          widgetButtons.forEach((i) => {
+            const widgetTemp = document.getElementById(i.dataset.target);
+            widgetTemp.style.display = "none";
+          }); 
+          targetWidget.style.display = "flex";
+        }
+
+        else{
+          targetWidget.style.display = "none";
+        }
       }
     });
   });
