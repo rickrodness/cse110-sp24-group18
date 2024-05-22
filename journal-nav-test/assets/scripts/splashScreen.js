d5db0e57c90cf38906a5e716f19e970adbd320be
BernicoJC <108056736+BernicoJC@users.noreply.github.com>
2024-05-21 23:29:22 -0700

    Fixed clippings, widget buttons, scrolling in splash

--- journal-nav-test/assets/scripts/splashScreen.js
+++ journal-nav-test/assets/scripts/splashScreen.js
@@ -1,6 +1,9 @@
 function attatchSplashListener() {
   const splashScreen = document.getElementById('splash-container');
+
   splashScreen.addEventListener('click', function(event) {
       splashScreen.classList.add('slide-up');
+      const style = document.getElementById("splashScreenLock");
+      style.disabled = true;
   })
 }
--- journal-nav-test/assets/scripts/widgets.js
+++ journal-nav-test/assets/scripts/widgets.js
@@ -1,36 +1,15 @@
 function widgetButtonListeners() {
-  const widgetContainer = document.getElementById('widget-buttons');
-  const widgetButtons = widgetContainer.getElementsByClassName('widget-btn');
-  const widgetPopoutContainer = document.getElementById('widget-popouts');
-  const widgetPopouts = widgetPopoutContainer.getElementsByClassName('widget');
+  const widgetButtons = document.querySelectorAll(".widget-btn");
 
-  for (let i = 0; i < widgetButtons.length; i++) {
-      const button = widgetButtons[i];
-      const widgetType = button.id;
-      let queryId = '';
-      switch (widgetType) {
-      case 'sleep-toggle-btn':
-          queryId = 'sleep-widget-container';
-          break;
-      case 'mood-toggle-btn':
-          queryId = 'mood-widget-container';
-          break;
-      default:
-          break;
+  console.log(widgetButtons)
+
+  widgetButtons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      const targetWidget = document.getElementById(btn.dataset.target);
+
+      if(targetWidget){
+        targetWidget.style.display = (targetWidget.style.display == "flex") ? "none":"flex";
       }
-      const assocWidget = document.getElementById(queryId);
-      button.addEventListener('click', function(event) {
-        if (assocWidget.style.zIndex == 1) {
-          button.style.filter = 'brightness(1)';
-          assocWidget.style.zIndex = -1;
-          return;
-        }
-        for (let j = 0; j < widgetPopouts.length; j++) {
-            widgetPopouts[j].style.zIndex = -1;
-            widgetButtons[j].style.filter = 'brightness(1)';
-        }
-        assocWidget.style.zIndex = 1;
-        button.style.filter = 'brightness(0.9)';
-      });
-  }
+    });
+  });
 }
